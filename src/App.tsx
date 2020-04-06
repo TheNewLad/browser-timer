3a95dc4967583bd5e35dc8f976878709d320663b
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2020-04-05 21:49:03 -0500

    Add react-dom and sass to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import './App.css';
+import './App.scss';
 import DateTimePicker from './main/components/DateTimePicker/DateTimePicker';
-import 'bulma/css/bulma.min.css';
+// import 'bulma/css/bulma.min.css';
+import Timer from './main/components/Timer/Timer';
 
 function App() {
   return (
+    <Router>
     <section className="section">
-      <div className="columns">
-        <div className="column">
-          <DateTimePicker />
-        </div>
-        <div className="column">2</div>
-      </div>
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <a href="/" className="navbar-item">OBT</a>
+            </div>
+            <div className="navbar-menu">
+                <Link className="navbar-item" to="/">Home</Link>
+                <Link className="navbar-item" to="/timer">Timer</Link>
+            </div>
+        </nav>
+
+        <Switch>
+            <Route exact path="/">
+                <DateTimePicker />
+            </Route>
+            <Route path="/timer">
+                <Timer />
+            </Route>
+        </Switch>
     </section>
+</Router>
   );
-}
+};
 
 export default App;
