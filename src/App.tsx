1ea5a550f820e304996ac1e809ae99da33f168a8
TheNewLad <22102599+TheNewLad@users.noreply.github.com>
2020-04-08 01:00:00 -0500

    Clean up datetime component

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,44 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import DateTimePicker from './main/components/DateTimePicker/DateTimePicker';
 import Timer from './main/components/Timer/Timer';
 import NavbarBurger from './main/components/NavbarBurger/NavbarBurger';
+import { HOME_PATH, TIMER_PATH } from './main/constants/constants';
 import './App.scss';
 const cx = require('classnames');
 
 
 function App() {
     const [isActive, setIsActive] = useState(false);
-    
+
     const toggleIsActive = () => setIsActive(!isActive);
 
-  return (
-    <Router>
-    <section className="section">
-        <nav className="navbar">
-            <div className="navbar-brand">
-                <a href="/" className="navbar-item">OBT</a>
-                <NavbarBurger 
-                    isActive={isActive}
-                    toggleIsActive={toggleIsActive}
-                />    
-            </div>
-            <div className={cx("navbar-menu", isActive ? "is-active" : "")}>
-                <Link className="navbar-item" to="/">Home</Link>
-                <Link className="navbar-item" to="/timer">Timer</Link>
-            </div>
-        </nav>
+    return (
+        <Router>
+            <section className="section">
+                <nav className="navbar">
+                    <div className="navbar-brand">
+                        <a href={HOME_PATH} className="navbar-item">OBS Browser Timer</a>
+                        <NavbarBurger
+                            isActive={isActive}
+                            toggleIsActive={toggleIsActive}
+                        />
+                    </div>
+                    <div className={cx("navbar-menu", isActive ? "is-active" : "")}>
+                        <Link className="navbar-item" to={HOME_PATH}>Home</Link>
+                        <Link className="navbar-item" to={TIMER_PATH}>Timer</Link>
+                    </div>
+                </nav>
 
-        <Switch>
-            <Route exact path="/">
-                <DateTimePicker />
-            </Route>
-            <Route path="/timer">
-                <Timer />
-            </Route>
-        </Switch>
-    </section>
-</Router>
-  );
+                <Switch>
+                    <Route exact path={HOME_PATH}>
+                        <DateTimePicker />
+                    </Route>
+                    <Route path={`${TIMER_PATH}/:datetime`}>
+                        <Timer/>
+                    </Route>
+                </Switch>
+            </section>
+        </Router>
+    );
 };
 
 export default App;
--- src/main/components/DateTimePicker/DateTimePicker.tsx
+++ src/main/components/DateTimePicker/DateTimePicker.tsx
@@ -1,29 +1,86 @@
-import React from 'react';
-
+import React, { useState, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import DateTime from 'react-datetime';
+import moment, { Moment } from 'moment';
+import { TIMER_PATH } from '../../constants/constants';
 
+import '@fortawesome/fontawesome-free/css/all.css';
 import './DateTime.css';
-import { Moment } from 'moment';
 
 const DateTimePicker = () => {
+    const [selectedDate, setSelectedDate] = useState<string | undefined>();
+    const [timerLink, setTimerLink] = useState<string | undefined>();
+    const [epochTime, setEpochTime] = useState<number | undefined>();
+
+    const location = removeTrailingSlash(window.location.href);
+    const history = useHistory();
+    const timerLinkInput = useRef<HTMLInputElement>(null);
+
     const handleChange = (event: string | Moment) => {
-        console.log(event.valueOf());
+        const eventMoment: Moment = moment(event).startOf('minute');
+        updateSelectedDate(eventMoment);
+        updateLinkToTimer(eventMoment.valueOf());
+        setEpochTime(eventMoment.valueOf());
     };
+    const updateSelectedDate = (moment: Moment) => {
+        setSelectedDate(moment.format('LLLL'));
+    };
+    const updateLinkToTimer = (event: number) => {
+        setTimerLink(`${location}${TIMER_PATH}/${event}`);
+    };
+    const copyLinkToClipboard = () => {
+        if (timerLinkInput.current !== null) {
+            timerLinkInput.current.select();
+            document.execCommand('copy');
+        }
+    };
+    const openTimerLink = () => epochTime ? history.push(`${TIMER_PATH}/${epochTime}`) : void (0);
 
     return (
         <div className="container">
             <div className="columns is-centered">
                 <div className="column">
-                    <DateTime
-                        className="has-text-centered"
-                        input={false}
-                        onChange={handleChange}
-                        defaultValue={new Date()}
-                    />
+                    <div className="container">
+
+                        <DateTime
+                            className="has-text-centered"
+                            input={false}
+                            onChange={handleChange}
+                            defaultValue={new Date()}
+                        />
+                    </div>
+                    <div className="container">
+                        <div className="field has-addons">
+                            <div className="control is-expanded">
+
+                                <input
+                                    ref={timerLinkInput}
+                                    className="input"
+                                    type="text"
+                                    placeholder="Generated Link (COPY ON CLICK)"
+                                    value={timerLink}
+                                    readOnly
+                                />
+                                <p className="help">{!selectedDate ? "" : `Selected Date: ${selectedDate}`}</p>
+
+                            </div>
+                            <div className="control">
+                                <a onClick={copyLinkToClipboard} className="button far fa-copy"></a>
+                            </div>
+                            <div className="control">
+                                <a onClick={openTimerLink} className="button fas fa-external-link-alt"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="column">
+                    {/* <TimerLink /> */}
                 </div>
             </div>
         </div>
     );
 };
+
+const removeTrailingSlash = (url: string): string => url.replace(/\/$/, "");
 
 export default DateTimePicker;
