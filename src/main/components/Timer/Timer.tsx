6757f1ff530c5cbe0f51f0a64d80e54ac4d5b440
devslow <22102599+devslow@users.noreply.github.com>
2020-04-14 22:03:11 -0500

    Add styling to Timer

--- src/main/components/Timer/Timer.tsx
+++ src/main/components/Timer/Timer.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import moment, { Moment, Duration } from 'moment';
 
+import styles from './Timer.module.css';
+
 type TimerTypes = {
     datetime: string
 }
@@ -23,18 +25,18 @@ const Timer = () => {
     let timerHtml;
     if (timeLeft) {
         timerHtml = (
-            <React.Fragment>
+            <span className="timer">
                 { timeLeft?.hours() !== 0 && <span className="hours">{ `${Math.floor(timeLeft?.asHours())}h ` }</span> }
                 { timeLeft?.minutes() !== 0 && <span className="minutes">{ `${timeLeft?.minutes()}m ` }</span> }
                 <span className="seconds">{ `${timeLeft?.seconds()}s` }</span>
-            </React.Fragment>
+            </span>
         );
     } else {
         timerHtml = (<React.Fragment />);
     }
 
     return (
-        <section className="stream-timer">
+        <section className={styles.container}>
             { timerHtml }
         </section>
     );
